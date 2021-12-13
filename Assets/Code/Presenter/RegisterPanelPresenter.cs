d3cdd7872cf9ff39c909dd0b0e374f8a74c5334d
ENTI\shanshan.yang <shanshan.yang@enti.cat>
2021-12-13 11:13:30 +0100

    Fixed some compilation bugs

--- Assets/Code/Presenter/RegisterPanelPresenter.cs
+++ Assets/Code/Presenter/RegisterPanelPresenter.cs
@@ -7,7 +7,7 @@
         _viewModel = viewModel;
                 
         var eventDispatcher = ServiceLocator.Instance.GetService<IEventDispatcherService>();
-        eventDispatcher.Subscribe<User>((data) =>
+        eventDispatcher.Subscribe<UserDto>((data) =>
         {
             _viewModel.IsVisible.Value = false;
         });
