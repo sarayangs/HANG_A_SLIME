4c2ea7024efe8ade6ef4ab9052b28800def0505d
ENTI\shanshan.yang <shanshan.yang@enti.cat>
2022-01-10 11:04:34 +0100

    ResultPopUpView

---
+++ Assets/Code/View/ResultPopupView.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UniRx;
+
+public class ResultPopupView : View
+{
+        [SerializeField] private Image _image;
+        [SerializeField] private TextMeshProUGUI _score;
+        [SerializeField] private TextMeshProUGUI _time;
+        [SerializeField] private RectTransform _buttonsParent;
+
+        [SerializeField] private GameObject _homeButtonPrefab;
+        [SerializeField] private GameObject _retryButtonPrefab;
+        [SerializeField] private GameObject _nextWordButtonPrefab;
+        [SerializeField] private Sprite _sadImage;
+        [SerializeField] private Sprite _happyImage;
+        
+        private ResultPopupViewModel _viewModel;
+
+        public void Setup(ResultPopupViewModel resultViewModel)
+        {
+                _viewModel = resultViewModel;
+
+                _viewModel.Win.Subscribe(hasWon =>
+                {
+                        if(hasWon)
+                        {
+                                _image.sprite = _happyImage;
+                                
+                        }
+                }).AddTo(_disposables);
+        }
+}
---
+++ Assets/Code/ViewModel/ResultPopupViewModel.cs
@@ -0,0 +1,10 @@
+﻿using UniRx;
+public class ResultPopupViewModel
+{
+    public readonly ReactiveProperty<bool> Win;
+
+    public ResultPopupViewModel()
+    {
+        Win = new ReactiveProperty<bool>();
+    }
+}
